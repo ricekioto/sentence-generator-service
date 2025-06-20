2bb76cdbd48f70f307db0e53d456094866c33852
mmkit <ataichukuev3@gmail.com>
2025-06-20 13:22:56 +0500

    init

---
+++ src/main/java/com/example/project_telegram_bot/SentenceGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceGeneratorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentenceGeneratorServiceApplication.class, args);
+    }
+
+}
