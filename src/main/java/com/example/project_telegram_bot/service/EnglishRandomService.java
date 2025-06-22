c1882b985520dab634e79e1c741fbc26ed5c5e56
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:54:59 +0500

    review

--- src/main/java/com/example/project_telegram_bot/service/EnglishRandomService.java
+++ src/main/java/com/example/project_telegram_bot/service/EnglishRandomService.java
@@ -8,28 +8,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EnglishRandomService {
-    @Value("${url.english.generate-website}")
+    @Value("${url.generate-website}")
     private String url;
     private final RequestService requestService;
-    private final ParsingService parsingService;
     private final BuildingUrlService buildingUrlService;
 
-    public EnglishRandomService(RequestService requestService, ParsingService parsingService, BuildingUrlService buildingUrlService) {
+    public EnglishRandomService(RequestService requestService, BuildingUrlService buildingUrlService) {
         this.requestService = requestService;
         this.buildingUrlService = buildingUrlService;
-        this.parsingService = parsingService;
     }
 
     public String getSentence() {
         String url = buildingUrlService.getGeneraterUrl();
         SentenceResponse response = requestService.getSentenceResponse(url);
-        if (response == null) {
-            throw new RandomSentenceServiceException("Не удалось получить ответ от " + url);
-        }
         String sentence = response.getContent();
-        if (sentence == null || sentence.trim().isEmpty() || sentence.equals("the element does not exist")) {
-            throw new RandomSentenceNotFoundException("Предложение не найдено на странице.");
-        }
         return sentence;
     }
 }
