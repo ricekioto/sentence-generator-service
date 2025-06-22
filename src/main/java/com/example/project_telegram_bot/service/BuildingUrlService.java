16509d20c005d9c079092870fbdafeb44ab65794
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:54:54 +0500

    review

--- src/main/java/com/example/project_telegram_bot/service/BuildingUrlService.java
+++ src/main/java/com/example/project_telegram_bot/service/BuildingUrlService.java
@@ -6,28 +6,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class BuildingUrlService {
-    @Value("${url.english.translate-website}")
-    private String translationUrl;
-    @Value("${url.english.generate-website}")
+    @Value("${url.generate-website}")
     private String generatorUrl;
-    private final String SOURCE_LANG = "en"; // Язык оригинала
-    private final String TARGET_LANG = "ru";
     private final String COUNT = "1";
     private final String LANGUAGE = "eng";
-
-
-    public String getTranslationUrl(String text) {
-        String encodedText = UriComponentsBuilder.fromUriString(text).build().encode().toString();
-
-        String url = UriComponentsBuilder.fromUriString(translationUrl)
-                .queryParam("source_lang", SOURCE_LANG)
-                .queryParam("target_lang", TARGET_LANG)
-                .queryParam("text", encodedText)
-                .build()
-                .toUriString();
-
-        return url;
-    }
 
     public String getGeneraterUrl() {
         String url = UriComponentsBuilder.fromUriString(generatorUrl)
