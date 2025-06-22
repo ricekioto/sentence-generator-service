12e15ec9adc97998eab4df5643cddf8c9603b7b1
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:54:43 +0500

    add security

---
+++ src/main/java/com/example/project_telegram_bot/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.project_telegram_bot.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+//    @Bean
+//    public SecurityFilterChain authorizeRequestsFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+//                .requestMatchers(GET, "/api/random/sentence").permitAll());
+//        return http.build();
+//
+//    }
+}
