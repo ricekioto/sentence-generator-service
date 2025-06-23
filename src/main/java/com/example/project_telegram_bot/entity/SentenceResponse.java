c0bec25d198a1a2f1bbb4987f857581d8949042d
mmkit <ataichukuev3@gmail.com>
2025-06-23 18:13:26 +0500

    review

--- src/main/java/com/example/project_telegram_bot/entity/SentenceResponse.java
+++ src/main/java/com/example/project_telegram_bot/entity/SentenceResponse.java
@@ -4,6 +4,7 @@ package com.example.project_telegram_bot.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SentenceResponse {
+
     private String content;
 
     @JsonProperty("content")
