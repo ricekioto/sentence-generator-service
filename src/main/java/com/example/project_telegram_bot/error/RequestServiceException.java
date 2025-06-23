34b4dbc62c5b7225e0e53d5ed721bde5e722732c
mmkit <ataichukuev3@gmail.com>
2025-06-23 18:13:17 +0500

    review

--- src/main/java/com/example/project_telegram_bot/error/RequestServiceException.java
+++ src/main/java/com/example/project_telegram_bot/error/RequestServiceException.java
@@ -2,11 +2,11 @@ package com.example.project_telegram_bot.error;
 
 public class RequestServiceException extends RuntimeException {
 
-  public RequestServiceException(String message) {
-    super(message);
-  }
+    public RequestServiceException(String message) {
+        super(message);
+    }
 
-  public RequestServiceException(String message, Throwable cause) {
-    super(message, cause);
-  }
+    public RequestServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
