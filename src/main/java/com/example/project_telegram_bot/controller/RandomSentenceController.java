d3a40c349706909c15049a751ef598512addf399
mmkit <ataichukuev3@gmail.com>
2025-06-24 12:22:06 +0500

    review

--- src/main/java/com/example/project_telegram_bot/controller/RandomSentenceController.java
+++ src/main/java/com/example/project_telegram_bot/controller/RandomSentenceController.java
@@ -2,6 +2,7 @@ package com.example.project_telegram_bot.controller;
 
 import com.example.project_telegram_bot.error.EnglishRandomServiceException;
 import com.example.project_telegram_bot.service.EnglishRandomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,15 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/random")
 public class RandomSentenceController {
 
     private final EnglishRandomService englishRandomService;
-
-    public RandomSentenceController(EnglishRandomService englishRandomService) {
-        this.englishRandomService = englishRandomService;
-    }
 
     @GetMapping("/sentence")
     public String getRandomSentenceEndpoint() throws EnglishRandomServiceException {
